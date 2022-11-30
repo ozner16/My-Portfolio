df7517f7366e14b29a6dd3bf2f5c9be12de50755
Renzo <ozner.dapmo@gmail.com>
2022-11-30 16:03:08 +0800

    page reload

--- script.js
+++ script.js
@@ -339,5 +339,20 @@ $(document).ready(function(){
       //    console.log($('.nav-small-screen').css('height'));
       //    console.log('window inner width: '+ window.innerWidth);
       //    console.log('window inner height: '+ window.innerHeight);
+      
+      var dwidth = jQuery(window).width();
+      jQuery(window).bind('resize', function(e){
+
+      var wwidth = jQuery(window).width();
+
+      if(dwidth!==wwidth){
+      dwidth = jQuery(window).width();
+
+      if (window.RT) clearTimeout(window.RT);
+      window.RT = setTimeout(function(){
+            this.location.reload(false); /* false to get page from cache */
+      }, 1000);
+      }
+      });
 
    });
